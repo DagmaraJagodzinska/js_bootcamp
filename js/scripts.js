d2c058d74b8a11976a462f94c9798e8e5ce113b2
Dagmara Jagodzinska <dag.jagodzinska@wp.pl>
2016-11-25 21:05:52 +0100

    pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a = prompt('Podaj a');
+var h = prompt('Podaj h');
+triangleArea = a*h/2;
+console.log ('Pole trójkąta o podstawie a= ' + a + ' i wysokości h= ' + h + ' wynosi ' +triangleArea);
