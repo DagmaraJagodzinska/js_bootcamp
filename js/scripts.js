11d84a3c40108dddfbc85417ff759c0828c3650f
Dagmara Jagodzinska <dag.jagodzinska@wp.pl>
2016-11-26 15:17:06 +0100

    Zadanie_8_5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // plik scripts.js
-
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
@@ -8,3 +7,15 @@ var a = prompt('Podaj a');
 var h = prompt('Podaj h');
 triangleArea = a*h/2;
 console.log ('Pole trójkąta o podstawie a= ' + a + ' i wysokości h= ' + h + ' wynosi ' +triangleArea);
+
+var a = prompt('Podaj a');
+var b = prompt('Podaj b');
+value = (a*a) + (2 * a * b) - (b*b);
+console.log('Wynik drugiego działania wynosi ' + value);
+
+if (value>0) {
+	console.log('Wynik jest dodatni');
+}
+else {
+	console.log('Wynik jest ujemny');
+}
